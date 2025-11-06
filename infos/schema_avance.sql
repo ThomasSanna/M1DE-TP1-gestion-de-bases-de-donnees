7c0c36238fe11d3d4a0fca551334a6e79b373ec4
ThomasSanna <thomassanna1e@gmail.com>
2025-11-06 16:51:22 +0100

    Ebauche rapport

--- infos/schema_avance.sql
+++ infos/schema_avance.sql
@@ -7,13 +7,13 @@
 
 -- 3.1. CRÉATION DES RÔLES
 
--- Rôle : Chercheur (accès restreint à ses projets et données)
+-- Rôle : Chercheur
 CREATE ROLE role_chercheur;
 
--- Rôle : Data Manager (accès élargi aux métadonnées)
+-- Rôle : Data Manager
 CREATE ROLE role_data_manager;
 
--- Rôle : Administrateur (accès complet)
+-- Rôle : Administrateur
 CREATE ROLE role_administrateur;
 
 -- 3.2. CRÉATION DES VUES
@@ -153,21 +153,34 @@ GROUP BY p.id, p.titre, p.discipline, p.budget_annuel_eur, p.date_debut, p.date_
 -- 3.3. ATTRIBUTION DES PRIVILÈGES
 
 -- Privilèges pour ADMINISTRATEUR (accès complet)
+GRANT USAGE ON SCHEMA univ_recherche TO role_data_manager;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA univ_recherche TO role_administrateur;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA univ_recherche TO role_administrateur;
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA univ_recherche TO role_administrateur;
 
 -- Privilèges pour DATA_MANAGER (accès élargi aux métadonnées)
+GRANT USAGE ON SCHEMA univ_recherche TO role_data_manager;
 GRANT SELECT ON ALL TABLES IN SCHEMA univ_recherche TO role_data_manager;
 GRANT INSERT, UPDATE, DELETE ON contrat, jeu_donnees TO role_data_manager;
 GRANT SELECT ON v_metadonnees_contrats, v_synthese_donnees_projet, v_tableau_bord_projets TO role_data_manager;
 GRANT SELECT ON v_publications_chercheur TO role_data_manager;
 
 -- Privilèges pour CHERCHEUR (accès restreint à ses projets et données)
+GRANT USAGE ON SCHEMA univ_recherche TO role_chercheur;
+
 GRANT SELECT ON projet, chercheur, laboratoire, institution, publication TO role_chercheur;
 GRANT SELECT ON v_mes_projets, v_mes_donnees, v_publications_chercheur TO role_chercheur;
 GRANT INSERT ON publication, publication_auteur, jeu_donnees TO role_chercheur;
 GRANT UPDATE ON jeu_donnees TO role_chercheur;
+
+-- 3.4. CRÉATION D'UN USER DATA_MANAGER et D'UN USER CHERCHEUR (EXEMPLES)
+-- Exemple de création d'un utilisateur Data Manager
+CREATE USER data_manager_user WITH PASSWORD 'motdepasse';
+GRANT role_data_manager TO data_manager_user;
+
+-- Exemple de création d'un utilisateur Chercheur
+CREATE USER "alexandrie.parent@dos.org" WITH PASSWORD 'motdepasse';
+GRANT role_chercheur TO "alexandrie.parent@dos.org";
 
 -- PARTIE 4 - REQUÊTES ET OPTIMISATION
 
